ca2cb4878bd1b792e1f7107f33d0443476109498
老师 <neko99tr@gmail.com>
2026-08-26 21:45:42 +0630

    Update website

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import MusicPlayer from "./components/MusicPlayer";
 import { Route, Routes, useLocation } from "react-router-dom";
@@ -8,30 +9,40 @@ import RoomDetails from "./pages/RoomDetails";
 import Facilities from "./pages/Facilities";
 import About from "./pages/About";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
+
+  return null;
+};
+
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <div className="min-h-screen bg-white text-gray-900 transition-colors duration-300 dark:bg-slate-950 dark:text-white">
+      <ScrollToTop />
 
       {!isOwnerPath && <Navbar />}
 
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/rooms" element={<AllRooms />} />
-
           <Route path="/rooms/:id" element={<RoomDetails />} />
-
           <Route path="/facilities" element={<Facilities />} />
-
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
 
       <MusicPlayer />
-
     </div>
   );
 };
